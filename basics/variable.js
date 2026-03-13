2f96495b7361d92c6b3127ec65dab61d8b695c76
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-13 23:46:16 +0530

    Variables Declaring

---
+++ basics/variable.js
@@ -0,0 +1,18 @@
+// Declaring different variables of different data types
+
+let firstName = 'Pratik'
+let lastName = 'Kuklare'
+let country = 'India'
+let city = 'Pune'
+let age = 20
+
+// Declaring variables with number values
+
+const gravity = 9.81
+const boilingPoint = 100
+const PI = 3.14
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Pratik',
+branch = 'CSE',
+live = 'Pune'
