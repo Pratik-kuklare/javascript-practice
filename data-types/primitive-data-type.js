e424f195f1bc9633392d868e87948c44615d1c4e
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-14 14:39:38 +0530

    Primitive data types

---
+++ data-types/primitive-data-type.js
@@ -0,0 +1,12 @@
+let numone = 5;
+let numtwo = 5;
+console.log(numone == numtwo); // true
+console.log(numone === numtwo); // true
+
+let firstName = 'Pratik';
+let lastName = 'Kuklare';
+console.log(firstName == lastName); // false
+
+let boolean1 = true;
+let boolean2 = false;
+console.log(boolean1 == boolean2); // false
