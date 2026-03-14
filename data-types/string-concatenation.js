d91e9b48cc4f92192c0ed90f06240fbdbf506d01
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-14 14:33:14 +0530

    Adding two or more variable using + operator

---
+++ data-types/string-concatenation.js
@@ -0,0 +1,7 @@
+// Concatenating using addition operator
+
+let firstName = 'Pratik';
+let lastName = 'Kuklare';
+let space = ' ';
+let fullName = firstName + space + lastName;
+console.log(fullName);
