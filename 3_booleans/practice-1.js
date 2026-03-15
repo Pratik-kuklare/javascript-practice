dd1dd07dab323ca251cb144cab55bf4d30b2ca47
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-15 21:03:50 +0530

    predict the output

---
+++ 3_booleans/practice-1.js
@@ -0,0 +1,9 @@
+// Predict the output
+
+console.log(5 === 5);
+console.log(5 == "5");
+console.log(5 === '5');
+console.log(Boolean(0));
+console.log(Boolean('hello'));
+console.log(!true);
+console.log(!!0)
