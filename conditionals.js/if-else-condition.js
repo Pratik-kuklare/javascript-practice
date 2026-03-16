9fb23ae39b92c818a8f345b6a1ac3e0a806fff04
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-16 21:41:18 +0530

    if-else conditional

---
+++ conditionals.js/if-else-condition.js
@@ -0,0 +1,27 @@
+let num = 3;
+if(num > 0) {
+    console.log(`${num} is a positive number`);
+} else {
+    console.log(`${num} is a negitive number`);
+}
+
+num = -3;
+if(num > 0) {
+    console.log(`${num} is a positive number`);
+} else {
+    console.log(`${num} is a negative number`);
+}
+
+let isRaining = true;
+if(isRaining) {
+    console.log('You need to bring a raincoat');
+} else {
+    console.log('No need to bring a raincoat');
+}
+
+isRaining = false;
+if(isRaining) {
+    console.log('You need to bring a raincoat');
+} else {
+    console.log('No need to bring a raincoat');
+}
