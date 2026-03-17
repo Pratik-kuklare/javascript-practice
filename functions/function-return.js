2b8838bc91d4ed11809f556a62c3d5064bcc27f7
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-17 23:21:53 +0530

    Function returning value

---
+++ functions/function-return.js
@@ -0,0 +1,16 @@
+function addTwoNumbers() {
+    let num1 = 20;
+    let num2 = 30;
+    let sum = num1 + num2;
+    return sum;
+}
+console.log(addTwoNumbers());
+
+function printFullName() {
+    let firstName = 'Pratik';
+    let lastName = 'Kuklare';
+    let space = ' ';
+    let fullName = firstName + space + lastName;
+    return fullName;
+}
+console.log(printFullName());
