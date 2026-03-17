6fae4aff96cfecb13ee3a143c1af1c4faf167c81
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-17 23:13:16 +0530

    Funtion without a Parameter and return

---
+++ functions/function-declartion.js
@@ -0,0 +1,30 @@
+function add() {
+    console.log(2 + 2);
+}
+add(); // calling a function
+
+// With variable
+
+function square() {
+    let num = 2;
+    let sq = num * num;
+    console.log(sq);
+}
+square(); // calling a function
+
+function addTwoNumbers() {
+    let numOne = 10;
+    let numTwo = 20;
+    let sum = numOne + numTwo;
+    console.log(sum);
+}
+addTwoNumbers();
+
+function printFullName() {
+    let firstName = 'Pratik';
+    let lastName = 'Kuklare';
+    let space = ' ';
+    let fullName = firstName + space + lastName;
+    console.log(fullName);
+}
+printFullName();
