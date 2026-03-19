0cf769d7e1751c9e5d09a8b37b5da9ce29b66984
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-19 23:16:05 +0530

    Creating new key for an object

---
+++ Objects/3.new-key.js
@@ -0,0 +1,14 @@
+let person3 = {
+     firstName : "Pratik",
+    lastName : "Kuklare",
+    age : 20,
+    skills : [
+        'HTML',
+        'CSS',
+        'JavaScript'
+    ],
+    isStudent : true
+}
+person3.newSkill = 'Java';
+console.log(person3);
+console.log(person3.newSkill);
