e6c5a30a16104ac7d7e7e54ca65d9bece26501d9
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-19 22:59:42 +0530

    Creating Object with Values

---
+++ Objects/objects-value.js
@@ -0,0 +1,35 @@
+// empty object
+const person = {};
+console.log(person);
+
+// object with value
+const person1 = {
+    name : "Pratik",
+    age : 20 
+};
+console.log(person1);
+
+const person2 = {
+    firstName : "Pratik",
+    lastName : "Kuklare",
+    age : 20,
+    skills : [
+        'HTML',
+        'CSS',
+        'JavaScript'
+    ]
+};
+console.log(person2);
+
+const person3 = {
+     firstName : "Pratik",
+    lastName : "Kuklare",
+    age : 20,
+    skills : [
+        'HTML',
+        'CSS',
+        'JavaScript'
+    ],
+    isStudent : true
+}
+console.log(person3);
