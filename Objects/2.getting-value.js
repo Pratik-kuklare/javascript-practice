c6e0e285dcf659363f9b8424ec9566ecf8420b2e
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-19 23:09:01 +0530

    Getting values from an object

---
+++ Objects/2.getting-value.js
@@ -0,0 +1,22 @@
+const person3 = {
+     firstName : "Pratik",
+    lastName : "Kuklare",
+    age : 20,
+    skills : [
+        'HTML',
+        'CSS',
+        'JavaScript'
+    ],
+    isStudent : true
+}
+// . dot notation method
+console.log(person3.firstName);
+console.log(person3.lastName);
+console.log(person3.age);
+console.log(person3.skills);
+
+// [] bracket notation method
+console.log(person3['firstName']);
+console.log(person3['lastName']);
+console.log(person3['age']);
+console.log(person3['skills']);
