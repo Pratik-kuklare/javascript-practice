67c803c6181649cce15117942bee74d96809b1c0
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-20 23:06:31 +0530

    Exercise 1

---
+++ Objects/practice-1.js
@@ -0,0 +1,23 @@
+// object practice
+const laptop = {
+    brand : "Samsung",
+    ram : 12,
+    price : 60000,
+    isAvailable : true
+};
+console.log(laptop);
+console.log(laptop.brand);
+console.log(laptop.ram);
+console.log(laptop. price);
+console.log(laptop.isAvailable);
+
+let student = {
+    name : "Pratik",
+    marks : 80,
+    cgpa : 8
+};
+student.college = "DY Patil";
+student.cgpa = 8.5;
+delete student.marks;
+console.log(student);
+
