ae7a033fa77ca4a591c39cb7bbfebffd454825ca
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-21 23:20:32 +0530

    Nested Object

---
+++ Objects/4.nested-object.js
@@ -0,0 +1,11 @@
+// creating new object inside object
+const school = {
+    name : "Dy Patil",
+    location : {
+        city : "Pune",
+        pincode : 411044
+    }
+};
+console.log(school);
+console.log(school.location.city);
+console.log(school.location.pincode);
