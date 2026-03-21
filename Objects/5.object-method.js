8ac3c78b1ddbed754f87e2c5b32ee3602d596625
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-21 23:26:54 +0530

    Function inside object

---
+++ Objects/5.object-method.js
@@ -0,0 +1,19 @@
+// creating function inside object
+const person = {
+    firstName : "Pratik",
+    lastName : "Kuklare",
+    age : 20,
+    getFullName : function() {
+        return `${person.firstName} ${person.lastName}`
+    }
+}
+console.log(person.getFullName())
+
+const calculator = {
+    brand: "Casio",
+    add: function(a, b) {
+        return a + b;
+    }
+};
+console.log(calculator.add(5, 3));
+console.log(calculator.brand);
