765d87945760ccff8424d9a854f2b499113c452d
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-24 23:55:46 +0530

    Getting Elements By ClassName

---
+++ DOM/2.elements-classname.js
@@ -0,0 +1,6 @@
+// Syntax
+document.getElementsByClassName('classname')
+
+const allTitles = document.getElementsByClassName('title');
+console.log(allTitles); // HTML Collections
+console.log(allTitles.length) // 4
