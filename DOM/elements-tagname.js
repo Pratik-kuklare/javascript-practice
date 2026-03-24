7dfe6c132870bf6618bca7d59257df79b71d0db0
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-24 23:52:16 +0530

    Getting Elements by TagName

---
+++ DOM/elements-tagname.js
@@ -0,0 +1,6 @@
+// Syntax
+document.getElementsByTagName('tagname')
+
+const allTitles = document.getElementByTagName('h1');
+console.log(allTitles); // HTML collections
+console.log(allTitles.length); // 4
