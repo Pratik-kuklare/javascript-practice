d65d295c6868f55a335370206ae01404c1e9de8c
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-25 00:00:24 +0530

    Getting Element by ID

---
+++ DOM/3.elements-id.js
@@ -0,0 +1,11 @@
+// Syntax
+document.getElementById('id')
+
+let firstTitle = document.getElementById('first-title');
+console.log(firstTitle); // <h1>First Title</h1>
+
+let secondTitle = document.getElementById('second-title');
+console.log(secondTitle); // <h1>Second Title</h1>
+
+let thirdTitle = document.getElementById('third-title');
+console.log(thirdTitle); // <h1>Third Title</h1>
