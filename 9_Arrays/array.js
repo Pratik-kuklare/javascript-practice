ec531363e9f45eaba45dfffb0a36c5ec34a398d2
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-26 23:35:29 +0530

    Creating array with values

---
+++ 9_Arrays/array.js
@@ -0,0 +1,32 @@
+const nums = [1, 2, 3, 4];
+console.log(nums);
+
+const fruits = ['Apple', 'Banana', 'cherry'];
+console.log(fruits);
+
+const mixed = [1, 'Apple', true, null, ''];
+console.log(mixed);
+
+const num = [
+    1,
+    2,
+    3,
+    4
+];
+console.log(num);
+
+const fruit = [
+    'Apple',
+    'Banana',
+    'cherry'
+];
+console.log(fruit);
+
+const mix = [
+    1,
+    'Apple',
+    true,
+    null,
+    ''
+];
+console.log(mix);
