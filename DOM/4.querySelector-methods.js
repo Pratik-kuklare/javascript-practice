b9be7fcd82a5ef9af4fe0c5b01488406883617c2
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-03-26 22:11:16 +0530

    Getting elements by using querySelector methods

---
+++ DOM/4.querySelector-methods.js
@@ -0,0 +1,5 @@
+// for selecting html or html elements by tag, name by id or class name.
+
+let firstTitle = document.querySelector('h1') // select the first available h1 element
+let firstTitle = document.querySelector('#first-title') // select id with first-title
+let firstTitle = document.querySelector('.title') // select the first available element with class title
