3e79cd7d4cc98b34a77f8612f953bf5d7c5cb289
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-04-02 18:46:57 +0530

    Exercise 1

---
+++ 10_Loops/3.practice.js
@@ -0,0 +1,19 @@
+// Print 1 to 10 Num
+
+for(i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Print 10 to 1(countdown)
+
+for(i = 10; i > 0; i--) {
+    console.log(i);
+}
+
+// Print 1 to 20 num using while
+
+let num = 1;
+while(num <= 20) {
+    console.log(num);
+    num++;
+}
