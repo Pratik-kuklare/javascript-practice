7c06589df3ab5b392d0903452607ab29c2ea76e7
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-04-02 18:18:04 +0530

    For Loop

---
+++ 10_Loops/for-loop.js
@@ -0,0 +1,14 @@
+// for loop structure
+/*
+for(intitialization, condition, increment/decrement) {
+    // code
+}
+*/
+
+for(let i = 0; i <= 5; i++) {
+    console.log(i);
+} // 0 1 2 3 4 5
+
+for(let i = 5; i >= 0; i--) {
+    console.log(i);
+}  // 5 4 3 2 1 0
